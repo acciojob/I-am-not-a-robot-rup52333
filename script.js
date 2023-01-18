20a8335d6dc9fec20be154c2f3e5a01bfe813bad
Rupkumar Sarkar <109466063+rup52333@users.noreply.github.com>
2023-01-18 11:00:05 +0530

    c

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+function getRandomImage() {  
+    //declare an array to store the images  
+    var randomImage = new Array();  
+      
+    //insert the URL of images in array  
+    randomImage[0] = "https://wi.wallpapertip.com/wsimgs/15-155208_desktop-puppy-wallpaper-hd.jpg";  
+    randomImage[1] = "http://www.petsworld.in/blog/wp-content/uploads/2014/09/running-cute-puppies.jpg";  
+    randomImage[2] = "https://wi.wallpapertip.com/wsimgs/156-1564365_golden-retriever-puppy-desktop-wallpaper-desktop-wallpaper-puppy.jpg";  
+    randomImage[3] = "https://wi.wallpapertip.com/wsimgs/156-1564140_free-puppy-wallpapers-for-computer-wallpaper-cave-cute.jpg";  
+    randomImage[4] = "https://wi.wallpapertip.com/wsimgs/156-1565522_puppies-desktop-wallpaper-desktop-background-puppies.jpg";  
+    randomImage[5] = "https://wi.wallpapertip.com/wsimgs/156-1566650_cute-puppies-desktop-wallpaper-cute-puppies.jpg";  
+      
+    //loop to display five randomly chosen images at once  
+    for (let i=0; i< 5; i++) {  
+    //generate a number and provide to the image to generate randomly  
+    var number = Math.floor(Math.random()*randomImage.length);  
+    
+
+    
+    } 
+    
+    
+    }  
